b7366903d674b7525a5de9f7d6752ee279e6a9f9
Zavtrax <60963899+Zavtrax@users.noreply.github.com>
2020-02-17 20:46:31 +0300

    Update Source.cpp
    5

--- First/Source.cpp
+++ First/Source.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main() {
 	cout << "Hello, world!";
-		cout << "I am doctor";
+		cout << "I am a doctor";
 		
-		cout << "I am doctor";
-		
+		cout << "I am a doctor";
+		cout << "I am not a doctor";
 		
 }
