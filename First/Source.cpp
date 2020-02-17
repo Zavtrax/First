85e00aa21d38c8c9e3b432143269ded2f6bb2ea8
Zavtrax <zavtrax007@mail.ru>
2020-02-17 22:22:28 +0300

    66

--- First/Source.cpp
+++ First/Source.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
 	cout << "Hello, world!";
 		cout << "I am a doctor";
-		
+		cout << "I am a doct";		
 		cout << "I am a doctor";
 		cout << "I am not a doctor";
 		
